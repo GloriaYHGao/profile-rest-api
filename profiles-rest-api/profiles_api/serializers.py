28fa35bad7819bb15469c3f734a648338f49c7c5
Gloria <gloria@eko.ai>
2020-02-12 23:08:54 +0800

    Added profile viewset

--- profiles-rest-api/profiles_api/serializers.py
+++ profiles-rest-api/profiles_api/serializers.py
@@ -1,6 +1,32 @@
 from rest_framework import serializers
 
+from profiles_api import models
+
 class HelloSerializer(serializers.Serializer):
     """Serializers a name field for testing our APIView"""
     name = serializers.CharField(max_length=10)
-    
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """Serializes a user profile object"""
+    # define a meta class that configure the serializer to point to a specific model (UserProfile model)using model serializer
+    class Meta:
+        model = models.UserProfile
+        fields = ('id', 'email', 'name', 'password')
+        # here we need to set an exception for password field because
+        #we only wanna use this when creating new user + no retrieving(write only)
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'style':{'input_type':'password'}
+            }
+        }
+
+    def create(self, validated_data):
+        """Create and return a new user"""
+        user = models.UserProfile.objects.create_user(
+            email = validated_data['email'],
+            name = validated_data['name'],
+            password = validated_data['password']
+        )
+
+        return user
--- profiles-rest-api/profiles_api/urls.py
+++ profiles-rest-api/profiles_api/urls.py
@@ -6,6 +6,7 @@ from profiles_api import views
 
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
+router.register('profile',views.UserProfileViewSet) #we dont need to specify base_name here because we have queryset in UserProfileViewSet
 
 urlpatterns = [
     path('hello-view/',views.HelloApiView.as_view()),
